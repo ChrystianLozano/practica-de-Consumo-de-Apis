02c27214e679ca133569b135e48bd4dbcee2cc8d
Chrystian Lozano <chrystianlozano@icloud.com>
2022-06-29 12:55:23 -0500

    seacrh

--- src/main.js
+++ src/main.js
@@ -80,3 +80,30 @@ const api = axios.create({
       genericSection.appendChild(movieContainer);
     });
   }
+
+
+  async function getMoviesBySearch(query) {
+    const { data } = await api('search/movie', {
+      params: {
+        query,
+      },
+    });
+    const movies = data.results;
+  
+    genericSection.innerHTML = "";
+    movies.forEach(movie => {
+      const movieContainer = document.createElement('div');
+      movieContainer.classList.add('movie-container');
+  
+      const movieImg = document.createElement('img');
+      movieImg.classList.add('movie-img');
+      movieImg.setAttribute('alt', movie.title);
+      movieImg.setAttribute(
+        'src',
+        'https://image.tmdb.org/t/p/w300' + movie.poster_path,
+      );
+  
+      movieContainer.appendChild(movieImg);
+      genericSection.appendChild(movieContainer);
+    });
+  }
